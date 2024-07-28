dfbf98f160188c9df3912739f70611bb50c5dea1
Daniiels098 <cl201117@g.unicamp.br>
2024-07-28 20:06:48 -0300

    Adicionando addons essenciais

--- Scripts/animacao_logo.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@onready var anim_C = $AnimationPlayer
-
-func _process(delta):
-	anim_C.play("anim_C")
--- Scripts/scene_trigger.gd
+++
@@ -1,5 +0,0 @@
-class_name SceneTrigger extends Area2D
-
-@export var connected_scene: String
-var scene_folder = "res://scenes/"
-
