12de3513064afcadd780c08adc616c38dec2fe26
Daniiels098 <cl201117@g.unicamp.br>
2024-08-30 17:36:09 -0300

    Remodelando cenas ao modo Zelda

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_button_pressed():
-	SceneManager.load_new_scene("res://scenes/menu.tscn")
+	SceneManager.load_new_scene("res://scenes/menu.tscn", "wipe_to_right")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,12 +2,11 @@ class_name Menu extends Control
 
 
 func _on_button_pressed():
-	#SceneManager.load_new_scene("res://scenes/corredor_terreo_academico.tscn")
-	SceneManager.load_new_scene("res://scenes/corredor_terreo_academico.tscn")
+	SceneManager.load_new_scene("res://scenes/corredor_terreo_academico.tscn", "wipe_to_right")
 
 func _on_button_4_pressed():
 	get_tree().quit()
 
 
 func _on_button_3_pressed():
-	SceneManager.load_new_scene("res://scenes/credits.tscn")
+	SceneManager.load_new_scene("res://scenes/credits.tscn", "wipe_to_right")
