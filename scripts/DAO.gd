f1408f98dafe1d24ae58cef4eb36e60322c6689f
Daniiels098 <cl201117@g.unicamp.br>
2024-09-21 18:41:27 -0300

    Add: Finalizando cena de login e iniciando script CRUD

---
+++ scripts/DAO.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name DAO
+
+func _ready():
+	db = SQLite.new
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- scripts/login_menu.gd
+++ scripts/login_menu.gd
@@ -5,19 +5,69 @@ var password
 var email
 var rePassword
 
-
 var created = false
 
-func _on_button_button_down():
+func toggle_register_login():
+	if $ToggleRegisterLogin.text == "Ja possui uma conta?":
+		#Register
+		$UsernameRe.visible = false
+		$NomeRe.visible = false
+		$EmailRe.visible = false
+		$PasswordRe.visible = false
+		$RePasswordRe.visible = false
+		$Registrar.visible = false
+		#Login
+		$Entrar.visible = true
+		$UsernameLo.visible = true
+		$PasswordLo.visible = true
+		$ToggleRegisterLogin.text = "Nao possui uma conta?"
+		$Titulo.text = "Entre na sua conta agora!"
+	elif $ToggleRegisterLogin.text == "Nao possui uma conta?":
+		#Register
+		$UsernameRe.visible = true
+		$NomeRe.visible = true
+		$EmailRe.visible = true
+		$PasswordRe.visible = true
+		$RePasswordRe.visible = true
+		$Registrar.visible = true
+		#Login
+		$Entrar.visible = false
+		$UsernameLo.visible = false
+		$PasswordLo.visible = false
+		#Buttons
+		$ToggleRegisterLogin.text = "Ja possui uma conta?"
+		$Titulo.text = "Salve seu progresso criando uma conta!"
+
+func _on_toggle_register_login_button_down():
+	toggle_register_login()
+	$Aviso.visible = false
+	$Aviso2.visible = false
+
+func _on_registrar_button_down():
+	$Aviso.visible = false
 	if !created:
-		username = $Username.text
-		password = $Password.text.sha256_text()
-		rePassword = $RePassword.text.sha256_text()
+		$Aviso2.visible = true
+		password = $PasswordRe.text
+		rePassword = $RePasswordRe.text
 		if password == rePassword:
-			print("OK")
+			$Aviso.visible = false
+			username = $UsernameRe.text
+			email = $EmailRe.text
+			password = $PasswordRe.text.sha256_text()
+			created = true
+			$UsernameRe.text = ""
+			$NomeRe.text = ""
+			$EmailRe.text = ""
+			$PasswordRe.text = ""
+			$RePasswordRe.text = ""
+			toggle_register_login()
 		else:
-			print("NOT OK")
-		created = true
-		$Button.text = "Login"
-		$Username.text = ""
-		$Password.text = ""
+			$Aviso.visible = true
+	if created:
+		$Aviso2.visible = true
+
+func _on_voltar_button_down():
+	SceneManager.load_new_scene("res://scenes/menu/menu.tscn")
+
+func _on_entrar_button_down():
+	pass
