f8fe2b4f858250beabeee9c96d6394d0def804bf
B-Francischetti <cl201024@g.unicamp.br>
2024-11-25 02:58:39 -0300

    ADD: Dialogos das missões, trigger da porta fechada, adicionando placas e trigger da missão do cecom

---
+++ quests/locals/fechado.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String
+const baloon = preload("res://dialogue/balloon.tscn")
+
+func _on_area_2d_body_entered(player: Player):
+	if not player is Player:
+		return
+	player.anim_exclama()
+	action()
+
+func action() -> void:
+	var ballon: Node = baloon.instantiate()
+	get_tree().current_scene.add_child(ballon)
+	ballon.start(dialogue_resource, dialogue_start)
