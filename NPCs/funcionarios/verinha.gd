afdf51d1c6977eb78bfd55df098e61b76c88358c
Daniiels098 <cl201117@g.unicamp.br>
2024-11-26 11:23:31 -0300

    ADD: Versão 1.3 do jogo

--- NPCs/funcionarios/verinha.gd
+++ NPCs/funcionarios/verinha.gd
@@ -8,8 +8,16 @@ const baloon = preload("res://dialogue/balloon.tscn")
 func _on_area_2d_body_entered(player: Player):
 	if not player is Player:
 		return
-	action()
+	if QuestsAt.is_quest_active("sala_vermelha"):
+		dialogue_start = "verinha1"
+	if QuestsAt.is_quest_active("sala_verde"):
+		dialogue_start = "verinha2"
+	if QuestsAt.is_quest_active("sala_amarela"):
+		dialogue_start = "verinha3"
+	if QuestsAt.is_quest_active("sala_azul"):
+		dialogue_start = "verinha4"
 	player.anim_interr()
+	action()
 
 func action() -> void:
 	var ballon: Node = baloon.instantiate()
